311f43ce8246d948728d1bd740e6048bb1e5bb4a
TheRealKS <koen.sauren@gmail.com>
2019-06-04 13:42:25 +0200

    Made the erver exit a little more gracefully

--- src/main/socketserver/Main.java
+++ src/main/socketserver/Main.java
@@ -29,9 +29,20 @@ public class Main {
         try {
             Configuration c = new Configuration();
             s = new SocketServer(new InetSocketAddress(c.getServerHost(), c.getServerPort()), c);
+            s.run();
         } catch (ParserConfigurationException | SAXException | IOException e) {
             e.printStackTrace();
+            System.exit(-1);
         }
-        s.run();
+
+            SocketServer finalS = s;
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                try {
+                    System.out.println("Shutting down...");
+                    finalS.stop();
+                } catch (IOException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }));
     }
 }
